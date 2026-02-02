7b6a15863beb45df4c3f0204fc239df2663a031d
Jeferson Leal <jeferson.gleal@outlook.com>
2026-02-02 19:58:44 -0300

    The initial project structure has been implemented.

---
+++ src/test/java/com/ensistec/ifood/api/IfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensistec.ifood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
