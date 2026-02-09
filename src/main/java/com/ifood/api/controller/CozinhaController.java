a43b8dead8bdae8f7b720cecdda7e689169bb6f2
Jeferson Leal <jeferson.gleal@outlook.com>
2026-02-09 16:31:34 -0300

    Implementado a consulta de singleton resource.

--- src/main/java/com/ifood/api/controller/CozinhaController.java
+++ src/main/java/com/ifood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		return repository.listar();
 	}
-	
+
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return repository.buscar(cozinhaId);
+	}
+
 }
