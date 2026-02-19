abfe88243db0300578168bbf894730a1343ad9b3
Jeferson Leal <jeferson.gleal@outlook.com>
2026-02-19 19:33:24 -0300

    Implementado a consulta por taxaFrete

--- src/main/java/com/ifood/api/controller/TesteController.java
+++ src/main/java/com/ifood/api/controller/TesteController.java
@@ -1,12 +1,15 @@
 package com.ifood.api.controller;
 
 import com.ifood.domain.model.Cozinha;
+import com.ifood.domain.model.Restaurante;
 import com.ifood.domain.repository.CozinhaRepository;
+import com.ifood.domain.repository.RestauranteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +20,10 @@ public class TesteController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+
+    @Autowired
+    private RestauranteRepository repository;
+
     @GetMapping("/cozinhas/por-nome")
     public List<Cozinha> cozinhasPorNome(String nome) {
         return cozinhaRepository.findTodasByNomeContaining(nome);
@@ -26,5 +33,12 @@ public class TesteController {
     public Optional<Cozinha> cozinhaPorNome(String nome) {
         return cozinhaRepository.findByNome(nome);
     }
+
+    @GetMapping("/restaurantes/por-taxa-frete")
+    public List<Restaurante> restaurantePorTaxaFrete(BigDecimal taxaInicial, BigDecimal taxaFinal) {
+        return repository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+
 
 }
--- src/main/java/com/ifood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/ifood/domain/repository/RestauranteRepository.java
@@ -1,10 +1,13 @@
 package com.ifood.domain.repository;
 
-import java.util.List;
-
 import com.ifood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
 }
