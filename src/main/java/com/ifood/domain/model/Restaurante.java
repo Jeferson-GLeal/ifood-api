41636253522339edf517e737cdc89f5194131be7
Jeferson Leal <jeferson.gleal@outlook.com>
2026-03-04 22:10:48 -0300

    6.2 - Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/ifood/domain/model/Restaurante.java
+++ src/main/java/com/ifood/domain/model/Restaurante.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -30,5 +32,11 @@ public class Restaurante {
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+		joinColumns = @JoinColumn(name = "restaurante_id"),
+		inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasDePagamentos = new ArrayList<>();
 	
 }
