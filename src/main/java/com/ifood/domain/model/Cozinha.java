8397812e021a704754b2a203666574b2ec2b7561
Jeferson Leal <jeferson.gleal@outlook.com>
2026-03-04 21:50:57 -0300

    6.1 - Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/ifood/domain/model/Cozinha.java
+++ src/main/java/com/ifood/domain/model/Cozinha.java
@@ -1,10 +1,14 @@
 package com.ifood.domain.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,5 +23,9 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+
+	//@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
